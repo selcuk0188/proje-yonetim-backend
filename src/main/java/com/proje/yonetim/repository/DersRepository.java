c76fa6165db7718af7b975627e5e139ecd40e9ae
selcuk0188 <74623494+selcuk0188@users.noreply.github.com>
2020-12-25 22:56:46 +0300

    Update DersRepository.java

--- src/main/java/com/proje/yonetim/repository/DersRepository.java
+++ src/main/java/com/proje/yonetim/repository/DersRepository.java
@@ -4,5 +4,5 @@ import com.proje.yonetim.entities.Ders;
 import com.proje.yonetim.entities.Proje;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DersRepository extends JpaRepository<Ders, Integer> {
+public interface DersRepository extends JpaRepository<Ders, Integer> {  // ders tablosu ile iletisim kuran repository
 }
